586ede7349e7cd0bb67c5d140bfd885d23de6f03
nightblood18 <114045738+nightblood18@users.noreply.github.com>
2024-08-20 22:03:07 -0500

    resolved route bug

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const userController = require('./../controllers/userController');
+const userController = require('./../controllers/usercontroller');
 const authController = require('./../controllers/authenticationController');
 
 const router = express.Router();
