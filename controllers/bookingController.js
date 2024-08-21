edfb88bec9b6058e35a6e64e163bd339eb1ff22d
nightblood18 <114045738+nightblood18@users.noreply.github.com>
2024-08-21 18:45:58 -0500

    testing stripe

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -9,28 +9,22 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.tourID);
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    line_items: [
-      {
-        price_data: {
-          currency: 'usd',
-          product_data: {
-            name: `${tour.name} Tour`,
-            description: tour.summary,
-            images: [
-              `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
-            ],
-          },
-          unit_amount: tour.price * 100, // Amount in cents
-        },
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    // success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourID}&user=${req.user.id}&price=${tour.price}`,
-    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourID,
+    line_items: [
+      {
+        name: `${tour.name} Tour`,
+        description: tour.summary,
+        images: [
+          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
+        ],
+        price: tour.price * 100,
+        currency: 'usd',
+        quantity: 1,
+      },
+    ],
   });
 
   res.status(200).json({
@@ -50,7 +44,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].price_data.unit_amount / 100;
+  const price = session.display_items[0].price / 100;
   await Booking.create({ tour, user, price });
 };
 
@@ -65,16 +59,15 @@ exports.webhookCheckout = (req, res, next) => {
       process.env.STRIPE_WEBHOOK_SECRET,
     );
   } catch (err) {
-    return res.status(400).send(`Webhook error:${err.message}`);
+    return res.status(400).send(`Webhook error: ${err.message}`);
   }
 
-  if (event.type === 'checkout.session.completed') {
+  if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
 
-    res.status(200).json({
-      received: true,
-    });
-  }
+  res.status(200).json({
+    received: true,
+  });
 };
 exports.createBooking = factory.createOne(Booking);
 exports.getBooking = factory.getOne(Booking);
