5ef24e3e638797b22f4b41c48bf32ff68ed55cef
nightblood18 <114045738+nightblood18@users.noreply.github.com>
2024-08-21 19:24:11 -0500

    testing stripe 5

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -59,12 +59,11 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].price / 100;
-  console.log(tour, user, price);
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
 
-exports.webhookCheckout = (req, res, next) => {
+exports.webhookCheckout = async (req, res, next) => {
   const signature = req.headers['stripe-signature'];
 
   let event;
@@ -79,7 +78,7 @@ exports.webhookCheckout = (req, res, next) => {
   }
 
   if (event.type === 'checkout.session.completed')
-    createBookingCheckout(event.data.object);
+    await createBookingCheckout(event.data.object);
 
   res.status(200).json({
     received: true,
