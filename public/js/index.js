8b71b482fa3657d642f45d30eabc9b4269a79401
nightblood18 <114045738+nightblood18@users.noreply.github.com>
2024-08-21 20:13:26 -0500

    Implemented stripe messages

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,7 @@ import { displayMap } from './mapbox';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
+import { showAlert } from './alerts';
 
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
@@ -64,3 +65,6 @@ if (bookBtn)
     const { tourId } = e.target.dataset;
     bookTour(tourId);
   });
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alert) showAlert('success', alertMessage, 20);
